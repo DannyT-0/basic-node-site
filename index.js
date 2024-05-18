610c728ccc673ac5f07bb20e706b4fd1bf6e3386
danny <dannytokarev@gmail.com>
2024-05-18 14:59:47 -0400

    refactored to remove redundancy and omitted mimetypes

--- index.js
+++ index.js
@@ -1,62 +1,31 @@
-var http = require("http");
-var fs = require("fs");
-var path = require("path");
+const http = require("http");
+const fs = require("fs");
 
-http
-	.createServer(function (req, res) {
-		let filePath = "." + req.url;
-		if (filePath == "./") {
-			filePath = "./index.html";
-		} else if (filePath == "./about") {
-			filePath = "./about.html";
-		} else if (filePath == "./contact-me") {
-			filePath = "./contact-me.html";
+const server = http.createServer((req, res) => {
+	let filePath = "." + req.url;
+	if (filePath === "./") {
+		filePath = "./index.html";
+	} else if (filePath === "./about") {
+		filePath = "./about.html";
+	} else if (filePath === "./contact-me") {
+		filePath = "./contact-me.html";
+	} else {
+		filePath = "./404.html";
+	}
+
+	fs.readFile(filePath, (error, content) => {
+		if (error) {
+			fs.readFile("./404.html", (error404, content404) => {
+				res.writeHead(200, { "Content-Type": "text/html" });
+				res.end(content404, "utf-8");
+			});
 		} else {
-			filePath = "./404.html";
+			res.writeHead(200, { "Content-Type": "text/html" });
+			res.end(content, "utf-8");
 		}
+	});
+});
 
-		const extname = String(path.extname(filePath)).toLowerCase();
-		const mimeTypes = {
-			".html": "text/html",
-			".js": "text/javascript",
-			".css": "text/css",
-			".json": "application/json",
-			".png": "image/png",
-			".jpg": "image/jpg",
-			".gif": "image/gif",
-			".wav": "audio/wav",
-			".mp4": "video/mp4",
-			".woff": "application/font-woff",
-			".ttf": "application/font-ttf",
-			".eot": "application/vnd.ms-fontobject",
-			".otf": "application/font-otf",
-			".svg": "application/image/svg+xml",
-		};
-
-		const contentType = mimeTypes[extname] || "application/octet-stream";
-
-		fs.readFile(filePath, function (error, content) {
-			if (error) {
-				if (error.code == "ENOENT") {
-					fs.readFile("./404.html", function (error, content) {
-						res.writeHead(200, { "Content-Type": "text/html" });
-						res.end(content, "utf-8");
-					});
-				} else {
-					res.writeHead(500);
-					res.end(
-						"Sorry, check with the site admin for error: " +
-							error.code +
-							" ..\n"
-					);
-					res.end();
-				}
-			} else {
-				res.writeHead(200, { "Content-Type": contentType });
-				res.end(content, "utf-8");
-			}
-		});
-	})
-	.listen(8080);
-
-console.log("Server running at http://localhost:8080/");
+server.listen(8080, () => {
+	console.log("Server running at http://localhost:8080/");
+});
